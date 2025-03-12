03dcb6867693a492bd7a2b46524a718d9e568487
鱼头星星 <13012447+fish-head-star-star@user.noreply.gitee.com>
2025-03-12 19:48:43 +0800

    YuXIN fix: booking  with status of pending

--- backendTest/DBTest/advanced.sql
+++ backendTest/DBTest/advanced.sql
@@ -6,14 +6,17 @@ CREATE TRIGGER update_availability_on_booking_insert
 AFTER INSERT ON Bookings
 FOR EACH ROW
 BEGIN
-    UPDATE Room_availability
-    SET availability = 2
-    WHERE room_id = NEW.room_id
-      AND available_date = NEW.booking_date
-      AND available_begin = NEW.start_time
-      AND available_end = NEW.end_time;
+    IF NEW.status = 'approved' THEN
+        UPDATE Room_availability
+        SET availability = 2
+        WHERE room_id = NEW.room_id
+          AND available_date = NEW.booking_date
+          AND available_begin = NEW.start_time
+          AND available_end = NEW.end_time;
+    END IF;
 END //
 DELIMITER ;
+
 
 -- 触发器：当 Bookings 表中记录的 status 字段更新为 'approved' 时，
 -- 更新对应 Room_availability 的 availability 为 2（已预订）。
